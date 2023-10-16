99114e31e84877beb83ebd7df99ede77f26b026e
DebayanSen96 <96684402+DebayanSen96@users.noreply.github.com>
2023-10-16 21:59:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000; // Use the provided PORT or default to 3000
 
 app.get("/", (req, res) => {
-  const data = { message: "Hello, World!" };
+  const data = { message: "Hi Debayan" };
   res.json(data);
 });
 
